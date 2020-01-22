18ef98f6e77a24e294d6baafd0d06af9d60f96d7
Perturabo4 <perturabo40@gmail.com>
2020-01-22 10:11:01 +0200

    add optimize-css-assets-webpack-plugin and terser-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,32 @@
+
+
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetsWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+    return config
+}
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -25,11 +46,7 @@ module.exports = {
             '@': path.resolve(__dirname, 'src')
         }
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
     devServer: {
         port: 3000
     },
@@ -60,7 +77,7 @@ module.exports = {
                 use: [{
                     loader: MiniCssExtractPlugin.loader,
                     options: {
-                        hrm: true,
+                        hrm: isDev,
                         reloadAll: true
                     }
                 }, 'css-loader']
@@ -80,7 +97,21 @@ module.exports = {
             {
                 test: /\.csv$/,
                 use: ['csv-loader']
-            }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hrm: isDev,
+                            reloadAll: true
+                        }
+                    }, 
+                    'css-loader',
+                    'less-loader'
+                ]
+            },
         ]
     }
 }
